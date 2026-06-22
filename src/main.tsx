f6596363931ee93a4552f8ee4d6524bddefd6bef
Nandana-008 <nandanakrnwn@gmail.com>
2026-06-22 09:47:40 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Homepage from './pages/Homepage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <RouterProvider router={router} />,
+)
